a6fd7d4ec1d19eeb3aa89a8dfee296ae1c421600
Vineet Mishra <vineet_mishra82@hotmail.com>
2023-07-22 20:30:09 +0530

    trying aprapi again

--- src/main/java/com/kpts/port23/Main.java
+++ src/main/java/com/kpts/port23/Main.java
@@ -14,6 +14,7 @@ import java.util.TreeMap;
 import com.aparapi.Kernel;
 import com.aparapi.Range;
 import com.aparapi.device.Device;
+import com.aparapi.device.Device.TYPE;
 import com.aparapi.internal.kernel.KernelManager;
 import com.aparapi.internal.kernel.KernelPreferences;
 
@@ -27,195 +28,215 @@ public class Main {
 
 	public static void main(String[] args) {
 
-	List<Map<String,String>> itemList = new ArrayList<Map<String,String>>();
-	System.out.println("Running strata project");
-	
-	try {
-		FileReader fileReader = new FileReader(args[0]);
-		
-		if(args.length==2)
-		{
-			loopCount = Double.valueOf(args[1]);
-			System.out.println("Per row loop count is "+loopCount);
-		}
-		else {
-			System.out.println("No loop count provided in argument, so per line loop will be taken into consideration.");
-		}
-		
-		if(fileReader!=null)
-		System.out.println("File found..start processing.. ");
-		
-		BufferedReader buffReader = new BufferedReader(fileReader);
-		
-		String line = "";
-		int count = 0;
-		while((line = buffReader.readLine())!=null)
-		{
-			String[] values = line.split(",");
-		
-			if(count==0)
-			{
-				for (String string : values) {
-					headers.add(string);
-					
-				}
-				
-				
-				System.out.println("headers size is "+headers.size());
-			}
-			
-			else if(values.length>0) {
-				
-				Map<String,String> item = new TreeMap<String, String>();
-				item.put("RowNo",String.valueOf(count));
-				for(int i = 0;i<headers.size();i++)
-				{
-					item.put(headers.get(i), values[i]);
-				}
-				
-				itemList.add(item);
-			}	
-			
-			
-			count++;
-		}
-		
-		if(itemList.size()>0)			
-		{
-			File file = new File("resultData.txt");
-			
-			PrintWriter writer = new PrintWriter(file);
-			writer.print("");
-			writer.close();
-			
-			
-			 long start = System.currentTimeMillis();	
-			 
-			 ProcessComputationOfData(itemList);		
-				 
-			 long end =  System.currentTimeMillis();
-			 System.out.println("Total time -> "+(end-start)+" milliseconds");
-		}
-		
-		buffReader.close();
-		
-	
-		
-	} catch (FileNotFoundException e) {
-		// TODO Auto-generated catch block
-	System.out.println("File not found at location...terminating");
-	System.exit(0);
-	} catch (IOException e) {
-		// TODO Auto-generated catch block
-	System.out.println("File reading error - "+e.getLocalizedMessage());
-	}
-	catch(NumberFormatException e)
-	{
-		loopCount = -1;
-	}
+		System.out.println("Program start");
+		  float[] input = new float[256];
+	        float[] output = new float[256];
 
-	}
+	        // Initialize input array
+	        
+	        for(int i = 0;i<input.length;i++)
+	        {
+	        	input[i] = i*3;
+	        }
 
-	private static void ProcessComputationOfData(List<Map<String, String>> itemList) {
-		System.out.println("File uploaded....");
-		int lineNo = 1;
-		
-		
-		 long start = System.currentTimeMillis();
-		 
-		for (Map<String,String> item : itemList) {
-			
-			List<Integer> loops = new ArrayList<Integer>();
-			
-			double loopSize = loopCount==-1 ? Integer.valueOf(item.get("Loops")) : loopCount;
-			
-			while(loopSize>0 && loopSize>Integer.MAX_VALUE-1 )
-			{
-				
-					double value = loopSize - Integer.MAX_VALUE-1;
-					
-					while(value>Integer.MAX_VALUE-1)
-					{
-						value -= Integer.MAX_VALUE-1;
-					}
-					
-					loopSize -= value;
-					
-					if(value>0)
-					{
-						loops.add((int)value);
-					}
-								
-			}
-			
-			loops.add((int) loopSize);
-			
-			//Checking loopsize
-			
-			for (Integer num : loops) {
-				
-				Product product = new 
-						Product(item.get("SECURITY_SCHEME")+","+item.get("SECURITY_VALUE"),
-								item.get("SECURITY_SCHEME")+","+item.get("ISSUER_VALUE"),
-								Long.valueOf(item.get("QUANTITY").replace("L", "")),
-								Double.valueOf(item.get("NOTIONAL")),
-								Double.valueOf(item.get("FIXED_RATE")),
-								item.get("START_DATE"),
-								item.get("END_DATE"),
-								item.get("SETTLEMENT"),
-								Double.valueOf(item.get("CLEAN_PRICE")),
-								item.get("VAL_DATE"),
-								item.get("RowNo")
-								);
-				
-				
-				Product.computeCount = 0;
-				
-				Range range = Range.create(num);
-				product.execute(range);
-							
-				System.out.println("Processed Row "+lineNo+" for "+num.toString()+" times and times computed (check) is "+Product.computeCount+" \n");
-				
-				product.dispose();
-				
-				System.out.println("Processed Row "+lineNo+" for "+num.toString()+" times and times computed (check) is "+Product.computeCount+" \n");
-				
-				
-				
-				lineNo++;
-			}
-						
-			StringBuilder data = new StringBuilder("");		
+	        Product kernel = new Product(input, output);
+	        kernel.execute(input.length);
+	        kernel.dispose();
+	        
+	        System.out.println("Output chk "+output[3]);
+	        System.out.println("Program end..output length -"+output.length);
+	        
 
-//			data.append("\n");	
-//			
-//			BufferedWriter buff;
-//			try {
-//				buff = new BufferedWriter(new FileWriter("resultData.txt",true));
-//			
-//			
-//			buff.write(data.toString());
+//	List<Map<String,String>> itemList = new ArrayList<Map<String,String>>();
+//	System.out.println("Running strata project");
+//	
+//	try {
+//		FileReader fileReader = new FileReader(args[0]);
+//		
+//		if(args.length==2)
+//		{
+//			loopCount = Double.valueOf(args[1]);
+//			System.out.println("Per row loop count is "+loopCount);
+//		}
+//		else {
+//			System.out.println("No loop count provided in argument, so per line loop will be taken into consideration.");
+//		}
+//		
+//		if(fileReader!=null)
+//		System.out.println("File found..start processing.. ");
+//		
+//		BufferedReader buffReader = new BufferedReader(fileReader);
+//		
+//		String line = "";
+//		int count = 0;
+//		while((line = buffReader.readLine())!=null)
+//		{
+//			String[] values = line.split(",");
+//		
+//			if(count==0)
+//			{
+//				for (String string : values) {
+//					headers.add(string);
+//					
+//				}
 //				
-//			buff.close();
-//			
-//			} catch (IOException e) {
-//				// TODO Auto-generated catch block
-//				e.printStackTrace();
+//				
+//				System.out.println("headers size is "+headers.size());
 //			}
-			
-			
-		}
-	
-		 
-				
-		//Writing to file
-//		System.out.println("Result data written to file -> resultData.txt");
-		
-		long end =  System.currentTimeMillis();
-		 
-//		System.out.println("\n\nTotal time(only process and file writing not reading) -> "+(end-start)+" milliseconds\n");
-		System.out.println("\n\nTotal time(only process and not file writing not reading) -> "+(end-start)+" milliseconds\n");
-		
+//			
+//			else if(values.length>0) {
+//				
+//				Map<String,String> item = new TreeMap<String, String>();
+//				item.put("RowNo",String.valueOf(count));
+//				for(int i = 0;i<headers.size();i++)
+//				{
+//					item.put(headers.get(i), values[i]);
+//				}
+//				
+//				itemList.add(item);
+//			}	
+//			
+//			
+//			count++;
+//		}
+//		
+//		if(itemList.size()>0)			
+//		{
+//			File file = new File("resultData.txt");
+//			
+//			PrintWriter writer = new PrintWriter(file);
+//			writer.print("");
+//			writer.close();
+//			
+//			
+//			 long start = System.currentTimeMillis();	
+//			 
+//			 ProcessComputationOfData(itemList);		
+//				 
+//			 long end =  System.currentTimeMillis();
+//			 System.out.println("Total time -> "+(end-start)+" milliseconds");
+//		}
+//		
+//		buffReader.close();
+//		
+//	
+//		
+//	} catch (FileNotFoundException e) {
+//		// TODO Auto-generated catch block
+//	System.out.println("File not found at location...terminating");
+//	System.exit(0);
+//	} catch (IOException e) {
+//		// TODO Auto-generated catch block
+//	System.out.println("File reading error - "+e.getLocalizedMessage());
+//	}
+//	catch(NumberFormatException e)
+//	{
+//		loopCount = -1;
+//	}
+//
+//	}
+//
+//	private static void ProcessComputationOfData(List<Map<String, String>> itemList) {
+//		System.out.println("File uploaded....");
+//		int lineNo = 1;
+//		
+//		
+//		 long start = System.currentTimeMillis();
+//		 
+//		for (Map<String,String> item : itemList) {
+//			
+//			List<Integer> loops = new ArrayList<Integer>();
+//			
+//			double loopSize = loopCount==-1 ? Integer.valueOf(item.get("Loops")) : loopCount;
+//			
+//			while(loopSize>0 && loopSize>Integer.MAX_VALUE-1 )
+//			{
+//				
+//					double value = loopSize - Integer.MAX_VALUE-1;
+//					
+//					while(value>Integer.MAX_VALUE-1)
+//					{
+//						value -= Integer.MAX_VALUE-1;
+//					}
+//					
+//					loopSize -= value;
+//					
+//					if(value>0)
+//					{
+//						loops.add((int)value);
+//					}
+//								
+//			}
+//			
+//			loops.add((int) loopSize);
+//			
+//			//Checking loopsize
+//			
+//			for (Integer num : loops) {
+//				
+//				Product product = new 
+//						Product(item.get("SECURITY_SCHEME")+","+item.get("SECURITY_VALUE"),
+//								item.get("SECURITY_SCHEME")+","+item.get("ISSUER_VALUE"),
+//								Long.valueOf(item.get("QUANTITY").replace("L", "")),
+//								Double.valueOf(item.get("NOTIONAL")),
+//								Double.valueOf(item.get("FIXED_RATE")),
+//								item.get("START_DATE"),
+//								item.get("END_DATE"),
+//								item.get("SETTLEMENT"),
+//								Double.valueOf(item.get("CLEAN_PRICE")),
+//								item.get("VAL_DATE"),
+//								item.get("RowNo")
+//								);
+//				
+//				
+//				Product.computeCount = 0;
+//				
+//				Device device = Device.bestGPU();
+//				Range range = device.createRange(num);
+//				product.execute(range);
+//							
+//				System.out.println("Processed Row "+lineNo+" for "+num.toString()+" times and times computed (check) is "+Product.computeCount+" \n");
+//				
+//				product.dispose();
+//				
+//				System.out.println("Processed Row "+lineNo+" for "+num.toString()+" times and times computed (check) is "+Product.computeCount+" \n");
+//				
+//				
+//				
+//				lineNo++;
+//			}
+//						
+//			StringBuilder data = new StringBuilder("");		
+//
+////			data.append("\n");	
+////			
+////			BufferedWriter buff;
+////			try {
+////				buff = new BufferedWriter(new FileWriter("resultData.txt",true));
+////			
+////			
+////			buff.write(data.toString());
+////				
+////			buff.close();
+////			
+////			} catch (IOException e) {
+////				// TODO Auto-generated catch block
+////				e.printStackTrace();
+////			}
+//			
+//			
+//		}
+//	
+//		 
+//				
+//		//Writing to file
+////		System.out.println("Result data written to file -> resultData.txt");
+//		
+//		long end =  System.currentTimeMillis();
+//		 
+////		System.out.println("\n\nTotal time(only process and file writing not reading) -> "+(end-start)+" milliseconds\n");
+//		System.out.println("\n\nTotal time(only process and not file writing not reading) -> "+(end-start)+" milliseconds\n");
+//		
 	}
 	 
 }
--- src/main/java/com/kpts/port23/Product.java
+++ src/main/java/com/kpts/port23/Product.java
@@ -53,199 +53,218 @@ import com.opengamma.strata.product.bond.ResolvedFixedCouponBondTrade;
 
 public class Product extends Kernel {
 
-	 private ReferenceData REF_DATA;	
-	  private SecurityId SECURITY_ID;
-	  private LegalEntityId ISSUER_ID;
-	  private long QUANTITY;
-	  private FixedCouponBondYieldConvention YIELD_CONVENTION;
-	  private double NOTIONAL;
-	  private double FIXED_RATE;
-	  private HolidayCalendarId EUR_CALENDAR;
-	  private DaysAdjustment DATE_OFFSET;
-	  private DayCount DAY_COUNT;
-	  private LocalDate START_DATE;
-	  private LocalDate END_DATE;
-	  private LocalDate VAL_DATE;
-	  private LocalDate SETTLEMENT;
-	  private BusinessDayAdjustment BUSINESS_ADJUST;
-	  private PeriodicSchedule PERIOD_SCHEDULE;
-	  private DaysAdjustment EX_COUPON;
-	  private double CLEAN_PRICE;
-	  private DiscountingFixedCouponBondTradePricer TRADE_PRICER;
-	  private DiscountingFixedCouponBondProductPricer PRODUCT_PRICER;
-	  private DiscountingPaymentPricer PRICER_NOMINAL;
-	  private ResolvedFixedCouponBond PRODUCT;
-	  double DIRTY_PRICE;
-	  private Payment UPFRONT_PAYMENT;
-	  private ResolvedFixedCouponBondTrade TRADE;
-	  private LegalEntityDiscountingProvider PROVIDER;
-	  private CurveInterpolator INTERPOLATOR = CurveInterpolators.LINEAR;
-	  private CurveName NAME_REPO = CurveName.of("TestRepoCurve");
-	  private CurveMetadata METADATA_REPO = Curves.zeroRates(NAME_REPO, ACT_365F);
-	  private InterpolatedNodalCurve CURVE_REPO = InterpolatedNodalCurve.of(
-	      METADATA_REPO, DoubleArray.of(0.1, 2.0, 10.0), DoubleArray.of(0.05, 0.06, 0.09), INTERPOLATOR);
-	  private RepoGroup GROUP_REPO = RepoGroup.of("GOVT1 BOND1");
-	  private LegalEntityGroup GROUP_ISSUER = LegalEntityGroup.of("GOVT1");
-	  private CurveName NAME_ISSUER = CurveName.of("TestIssuerCurve");
-	  private CurveMetadata METADATA_ISSUER = Curves.zeroRates(NAME_ISSUER, ACT_365F);
-	  private InterpolatedNodalCurve CURVE_ISSUER = InterpolatedNodalCurve.of(
-		      METADATA_ISSUER, DoubleArray.of(0.2, 9.0, 15.0), DoubleArray.of(0.03, 0.05, 0.13), INTERPOLATOR);
-	  
-	  public String rowNum;
-	  
-	  public boolean isprinted = false;
-	  
-	  public static double computeCount;
-	  
-	  public Product(String sECURITY_ID, String iSSUER_ID, long qUANTITY,
-			double nOTIONAL, double fIXED_RATE,
-			String sTART_DATE,
-			String eND_DATE,String settlementDate,double cleanPrice,String valDate,String rowNum) {
-		SECURITY_ID = getSecurityID(sECURITY_ID);
-		ISSUER_ID = getLegalEntityID(iSSUER_ID);
-		QUANTITY = qUANTITY;
-		YIELD_CONVENTION  = FixedCouponBondYieldConvention.DE_BONDS;
-		NOTIONAL = nOTIONAL;
-		FIXED_RATE = fIXED_RATE;
-		EUR_CALENDAR = HolidayCalendarIds.EUTA;
-		DATE_OFFSET = DaysAdjustment.ofBusinessDays(3, EUR_CALENDAR);
-		DAY_COUNT = DayCounts.ACT_365F;
-		START_DATE = getLocalDate(sTART_DATE);
-		END_DATE = getLocalDate(eND_DATE);
-		SETTLEMENT = getLocalDate(settlementDate);
-		BUSINESS_ADJUST =
-			      BusinessDayAdjustment.of(BusinessDayConventions.MODIFIED_FOLLOWING, EUR_CALENDAR);
-		PERIOD_SCHEDULE = PeriodicSchedule.of(
-			      START_DATE, END_DATE, Frequency.P6M, BUSINESS_ADJUST, StubConvention.SHORT_INITIAL, false);
-		EX_COUPON = DaysAdjustment.ofCalendarDays(-5, BUSINESS_ADJUST);
-		CLEAN_PRICE = cleanPrice;
-		VAL_DATE = getLocalDate(valDate);
-		TRADE_PRICER  = DiscountingFixedCouponBondTradePricer.DEFAULT;
-		PRODUCT_PRICER = TRADE_PRICER.getProductPricer();
-		REF_DATA = ReferenceData.standard();
-		PRICER_NOMINAL = DiscountingPaymentPricer.DEFAULT;
-		CalculateProduct();
-		DIRTY_PRICE  = PRODUCT_PRICER.dirtyPriceFromCleanPrice(PRODUCT, SETTLEMENT, CLEAN_PRICE);
-		UPFRONT_PAYMENT = Payment.of(
-			      CurrencyAmount.of(EUR, -QUANTITY * NOTIONAL * DIRTY_PRICE), SETTLEMENT);
-		CalculateTrade();
-		PROVIDER  = createRatesProvider(VAL_DATE);
-		this.rowNum = rowNum;
-	  }
-
-	private LegalEntityDiscountingProvider createRatesProvider(LocalDate valuationDate) {
-	    DiscountFactors dscRepo = ZeroRateDiscountFactors.of(EUR, valuationDate, CURVE_REPO);
-	    DiscountFactors dscIssuer = ZeroRateDiscountFactors.of(EUR, valuationDate, CURVE_ISSUER);
-	    LegalEntityDiscountingProvider provider = ImmutableLegalEntityDiscountingProvider.builder()
-	        .issuerCurves(ImmutableMap.of(Pair.of(GROUP_ISSUER, EUR), dscIssuer))
-	        .issuerCurveGroups(ImmutableMap.of(ISSUER_ID, GROUP_ISSUER))
-	        .repoCurves(ImmutableMap.of(Pair.of(GROUP_REPO, EUR), dscRepo))
-	        .repoCurveSecurityGroups(ImmutableMap.of(SECURITY_ID, GROUP_REPO))
-	        .valuationDate(valuationDate)
-	        .build();
-	    return provider;
-	  }
-
-	private void CalculateTrade() {
-		TRADE = ResolvedFixedCouponBondTrade.builder()
-			      .product(PRODUCT)
-			      .quantity(QUANTITY)
-			      .settlement(ResolvedFixedCouponBondSettlement.of(SETTLEMENT, CLEAN_PRICE))
-			      .build();
-		
-	}
-
-	private void CalculateProduct() {
-		
-		PRODUCT = FixedCouponBond.builder()
-			      .securityId(SECURITY_ID)
-			      .dayCount(DAY_COUNT)
-			      .fixedRate(FIXED_RATE)
-			      .legalEntityId(ISSUER_ID)
-			      .currency(EUR)
-			      .notional(NOTIONAL)
-			      .accrualSchedule(PERIOD_SCHEDULE)
-			      .settlementDateOffset(DATE_OFFSET)
-			      .yieldConvention(YIELD_CONVENTION)
-			      .exCouponPeriod(EX_COUPON)
-			      .build()
-			      .resolve(REF_DATA);
-		
-	}
-
-	private LocalDate getLocalDate(String sTART_DATE2) {
-		
-		String[] values = sTART_DATE2.split("-");
-		return LocalDate.of(Integer.valueOf(values[2]),
-				Integer.valueOf(values[1]),
-				Integer.valueOf(values[0]));
-	}
-
-	private LegalEntityId getLegalEntityID(String iSSUER_ID2) {
-		String[] values = iSSUER_ID2.split(",");
-		
-		return LegalEntityId.of(values[0],values[1]);
-	}
-
-	private SecurityId getSecurityID(String sECURITY_ID2) {
-		
-		String[] values = sECURITY_ID2.split(",");
-		
-		return SecurityId.of(values[0],values[1]);
-	}
+//	 private ReferenceData REF_DATA;	
+//	  private SecurityId SECURITY_ID;
+//	  private LegalEntityId ISSUER_ID;
+//	  private long QUANTITY;
+//	  private FixedCouponBondYieldConvention YIELD_CONVENTION;
+//	  private double NOTIONAL;
+//	  private double FIXED_RATE;
+//	  private HolidayCalendarId EUR_CALENDAR;
+//	  private DaysAdjustment DATE_OFFSET;
+//	  private DayCount DAY_COUNT;
+//	  private LocalDate START_DATE;
+//	  private LocalDate END_DATE;
+//	  private LocalDate VAL_DATE;
+//	  private LocalDate SETTLEMENT;
+//	  private BusinessDayAdjustment BUSINESS_ADJUST;
+//	  private PeriodicSchedule PERIOD_SCHEDULE;
+//	  private DaysAdjustment EX_COUPON;
+//	  private double CLEAN_PRICE;
+//	  private DiscountingFixedCouponBondTradePricer TRADE_PRICER;
+//	  private DiscountingFixedCouponBondProductPricer PRODUCT_PRICER;
+//	  private DiscountingPaymentPricer PRICER_NOMINAL;
+//	  private ResolvedFixedCouponBond PRODUCT;
+//	  double DIRTY_PRICE;
+//	  private Payment UPFRONT_PAYMENT;
+//	  private ResolvedFixedCouponBondTrade TRADE;
+//	  private LegalEntityDiscountingProvider PROVIDER;
+//	  private CurveInterpolator INTERPOLATOR = CurveInterpolators.LINEAR;
+//	  private CurveName NAME_REPO = CurveName.of("TestRepoCurve");
+//	  private CurveMetadata METADATA_REPO = Curves.zeroRates(NAME_REPO, ACT_365F);
+//	  private InterpolatedNodalCurve CURVE_REPO = InterpolatedNodalCurve.of(
+//	      METADATA_REPO, DoubleArray.of(0.1, 2.0, 10.0), DoubleArray.of(0.05, 0.06, 0.09), INTERPOLATOR);
+//	  private RepoGroup GROUP_REPO = RepoGroup.of("GOVT1 BOND1");
+//	  private LegalEntityGroup GROUP_ISSUER = LegalEntityGroup.of("GOVT1");
+//	  private CurveName NAME_ISSUER = CurveName.of("TestIssuerCurve");
+//	  private CurveMetadata METADATA_ISSUER = Curves.zeroRates(NAME_ISSUER, ACT_365F);
+//	  private InterpolatedNodalCurve CURVE_ISSUER = InterpolatedNodalCurve.of(
+//		      METADATA_ISSUER, DoubleArray.of(0.2, 9.0, 15.0), DoubleArray.of(0.03, 0.05, 0.13), INTERPOLATOR);
+//	  
+//	  public String rowNum;
+//	  
+//	  public boolean isprinted = false;
+//	  
+//	  public static double computeCount;
+//	  
+//	  public Product(String sECURITY_ID, String iSSUER_ID, long qUANTITY,
+//			double nOTIONAL, double fIXED_RATE,
+//			String sTART_DATE,
+//			String eND_DATE,String settlementDate,double cleanPrice,String valDate,String rowNum) {
+//		SECURITY_ID = getSecurityID(sECURITY_ID);
+//		ISSUER_ID = getLegalEntityID(iSSUER_ID);
+//		QUANTITY = qUANTITY;
+//		YIELD_CONVENTION  = FixedCouponBondYieldConvention.DE_BONDS;
+//		NOTIONAL = nOTIONAL;
+//		FIXED_RATE = fIXED_RATE;
+//		EUR_CALENDAR = HolidayCalendarIds.EUTA;
+//		DATE_OFFSET = DaysAdjustment.ofBusinessDays(3, EUR_CALENDAR);
+//		DAY_COUNT = DayCounts.ACT_365F;
+//		START_DATE = getLocalDate(sTART_DATE);
+//		END_DATE = getLocalDate(eND_DATE);
+//		SETTLEMENT = getLocalDate(settlementDate);
+//		BUSINESS_ADJUST =
+//			      BusinessDayAdjustment.of(BusinessDayConventions.MODIFIED_FOLLOWING, EUR_CALENDAR);
+//		PERIOD_SCHEDULE = PeriodicSchedule.of(
+//			      START_DATE, END_DATE, Frequency.P6M, BUSINESS_ADJUST, StubConvention.SHORT_INITIAL, false);
+//		EX_COUPON = DaysAdjustment.ofCalendarDays(-5, BUSINESS_ADJUST);
+//		CLEAN_PRICE = cleanPrice;
+//		VAL_DATE = getLocalDate(valDate);
+//		TRADE_PRICER  = DiscountingFixedCouponBondTradePricer.DEFAULT;
+//		PRODUCT_PRICER = TRADE_PRICER.getProductPricer();
+//		REF_DATA = ReferenceData.standard();
+//		PRICER_NOMINAL = DiscountingPaymentPricer.DEFAULT;
+//		CalculateProduct();
+//		DIRTY_PRICE  = PRODUCT_PRICER.dirtyPriceFromCleanPrice(PRODUCT, SETTLEMENT, CLEAN_PRICE);
+//		UPFRONT_PAYMENT = Payment.of(
+//			      CurrencyAmount.of(EUR, -QUANTITY * NOTIONAL * DIRTY_PRICE), SETTLEMENT);
+//		CalculateTrade();
+//		PROVIDER  = createRatesProvider(VAL_DATE);
+//		this.rowNum = rowNum;
+//	  }
+//
+//	private LegalEntityDiscountingProvider createRatesProvider(LocalDate valuationDate) {
+//	    DiscountFactors dscRepo = ZeroRateDiscountFactors.of(EUR, valuationDate, CURVE_REPO);
+//	    DiscountFactors dscIssuer = ZeroRateDiscountFactors.of(EUR, valuationDate, CURVE_ISSUER);
+//	    LegalEntityDiscountingProvider provider = ImmutableLegalEntityDiscountingProvider.builder()
+//	        .issuerCurves(ImmutableMap.of(Pair.of(GROUP_ISSUER, EUR), dscIssuer))
+//	        .issuerCurveGroups(ImmutableMap.of(ISSUER_ID, GROUP_ISSUER))
+//	        .repoCurves(ImmutableMap.of(Pair.of(GROUP_REPO, EUR), dscRepo))
+//	        .repoCurveSecurityGroups(ImmutableMap.of(SECURITY_ID, GROUP_REPO))
+//	        .valuationDate(valuationDate)
+//	        .build();
+//	    return provider;
+//	  }
+//
+//	private void CalculateTrade() {
+//		TRADE = ResolvedFixedCouponBondTrade.builder()
+//			      .product(PRODUCT)
+//			      .quantity(QUANTITY)
+//			      .settlement(ResolvedFixedCouponBondSettlement.of(SETTLEMENT, CLEAN_PRICE))
+//			      .build();
+//		
+//	}
+//
+//	private void CalculateProduct() {
+//		
+//		PRODUCT = FixedCouponBond.builder()
+//			      .securityId(SECURITY_ID)
+//			      .dayCount(DAY_COUNT)
+//			      .fixedRate(FIXED_RATE)
+//			      .legalEntityId(ISSUER_ID)
+//			      .currency(EUR)
+//			      .notional(NOTIONAL)
+//			      .accrualSchedule(PERIOD_SCHEDULE)
+//			      .settlementDateOffset(DATE_OFFSET)
+//			      .yieldConvention(YIELD_CONVENTION)
+//			      .exCouponPeriod(EX_COUPON)
+//			      .build()
+//			      .resolve(REF_DATA);
+//		
+//	}
+//
+//	private LocalDate getLocalDate(String sTART_DATE2) {
+//		
+//		String[] values = sTART_DATE2.split("-");
+//		return LocalDate.of(Integer.valueOf(values[2]),
+//				Integer.valueOf(values[1]),
+//				Integer.valueOf(values[0]));
+//	}
+//
+//	private LegalEntityId getLegalEntityID(String iSSUER_ID2) {
+//		String[] values = iSSUER_ID2.split(",");
+//		
+//		return LegalEntityId.of(values[0],values[1]);
+//	}
+//
+//	private SecurityId getSecurityID(String sECURITY_ID2) {
+//		
+//		String[] values = sECURITY_ID2.split(",");
+//		
+//		return SecurityId.of(values[0],values[1]);
+//	}
+//	
+//	public String calculatePresentValue() {
+//	    CurrencyAmount computedTrade = TRADE_PRICER.presentValue(TRADE, PROVIDER);
+//	    CurrencyAmount computedProduct = PRODUCT_PRICER.presentValue(PRODUCT, PROVIDER);
+//	    CurrencyAmount pvPayment =
+//	    PRICER_NOMINAL.presentValue(UPFRONT_PAYMENT, ZeroRateDiscountFactors.of(EUR, VAL_DATE, CURVE_REPO));
+//	    
+//	    String str = "";
+//	    str+="Present Values ->";
+//	    str+="\nComputed Trade - "+computedTrade.getCurrency()+" : "+computedTrade.getAmount();
+//	    str+="\nComputed Product - "+computedProduct.getCurrency()+" : "+computedProduct.getAmount();
+//	    str+="\nPv Payment - "+pvPayment.getCurrency()+" : "+pvPayment.getAmount();
+//	    
+//	    
+////	    if(!isprinted)
+////	    {
+////	    	System.out.println("\nPrinting for Row "+rowNum);
+////	    	System.out.println(str);
+////	    	isprinted = true;
+////	    }
+//	    
+//	  return str;
+//	   
+//	}
+//
+//	@Override
+//	public void run() {
+//		
+//		calculatePresentValue();
+//		// computeCount = getGlobalId();  
+////	WriteToFile(str);
+//	
+//	}
+//
+//	private void WriteToFile(String str) {
+//		  try {
+//		    	
+//		    	BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter("resultData.txt",true));
+//		    	
+//		    	bufferedWriter.append(str);
+//		    	
+//		    	bufferedWriter.close();
+//		    	
+//		    }catch(FileNotFoundException ex)
+//		    {
+//		    	System.out.println("Target file not found to write");
+//		    }
+//		    catch(IOException ex)
+//		    {
+//		    	System.out.println("Could not write to file...");
+//		    }
+//		
+//	}
 	
-	public String calculatePresentValue() {
-	    CurrencyAmount computedTrade = TRADE_PRICER.presentValue(TRADE, PROVIDER);
-	    CurrencyAmount computedProduct = PRODUCT_PRICER.presentValue(PRODUCT, PROVIDER);
-	    CurrencyAmount pvPayment =
-	    PRICER_NOMINAL.presentValue(UPFRONT_PAYMENT, ZeroRateDiscountFactors.of(EUR, VAL_DATE, CURVE_REPO));
-	    
-	    String str = "";
-	    str+="Present Values ->";
-	    str+="\nComputed Trade - "+computedTrade.getCurrency()+" : "+computedTrade.getAmount();
-	    str+="\nComputed Product - "+computedProduct.getCurrency()+" : "+computedProduct.getAmount();
-	    str+="\nPv Payment - "+pvPayment.getCurrency()+" : "+pvPayment.getAmount();
-	    
-	    
-//	    if(!isprinted)
-//	    {
-//	    	System.out.println("\nPrinting for Row "+rowNum);
-//	    	System.out.println(str);
-//	    	isprinted = true;
-//	    }
-	    
-	  return str;
-	   
-	}
+	 private float[] input;
+	    private float[] output;
 
-	@Override
-	public void run() {
-		
-		calculatePresentValue();
-		// computeCount = getGlobalId();  
-//	WriteToFile(str);
-	
-	}
+	    public Product(float[] input, float[] output) {
+	        this.input = input;
+	        this.output = output;
+	    }
 
-	private void WriteToFile(String str) {
-		  try {
-		    	
-		    	BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter("resultData.txt",true));
-		    	
-		    	bufferedWriter.append(str);
-		    	
-		    	bufferedWriter.close();
-		    	
-		    }catch(FileNotFoundException ex)
-		    {
-		    	System.out.println("Target file not found to write");
-		    }
-		    catch(IOException ex)
-		    {
-		    	System.out.println("Could not write to file...");
-		    }
-		
-	}
+	    @Override
+	    public void run() {
+	        int gid = getGlobalId();
+	        output[gid] = someFunction(input[gid]);
+	    }
+
+	    private float someFunction(float x) {
+			return x;
+	        // Perform some computationally intensive operation on x
+	    }
 	  
 	  
 }
